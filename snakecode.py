0dbf1275fceaedb8ecf079a9a663ab3b2b7bdee0
Katie Wheeler <wheel2kl@dukes.jmu.edu>
2020-12-10 03:32:30 -0800

    requirements

---
+++ snakecode.py
@@ -0,0 +1,62 @@
+# Snake Python
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object) :
+    rows = 0
+    w = 0
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)) :
+        pass
+
+    def move(self, dirnx, dirny) :
+        pass
+    def draw(self, surface, eyes=False) :
+        pass
+
+
+class snake(object) :
+    def __init__self, color, pos) :
+        pass
+
+    def move(self) :
+        pass
+
+    def reset(self, pos) :
+        pass
+    def addCube(self) :
+        pass
+
+    def draw(self, surface) :
+        pass
+
+
+def drawGrid(w, rows, surface) :
+    pass
+
+def redrawWindow(surfaxce) :
+    pass
+
+def randomSnack(rows, items) :
+    pass
+
+def message_bow(subject, content) :
+    pass
+
+def main() :
+    width =
+    height =
+    win =
+    pass
+
+
+rows =
+w =
+h =
+
+cube.rows = rows
+cube.w = w
+main()
